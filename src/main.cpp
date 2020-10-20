a52d42dfa8975c7e3d073c6b2ac0c9e6e32e3f01
Alexander U.Jurankov <heir@mail.ru>
2020-10-20 17:40:50 +0300

    Add `zlib` and `microtar` libraries

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,11 @@
 #include <filesystem>
 #include <logger.hpp>
 #include <memory>
+#include <microtar.h>
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <zlib.h>
 
 ///
 /// Download any file from the Internet
@@ -63,6 +65,50 @@ try {
         auto db_tar_gz = get_file(repo_url + '/' + repo_name + ".db.tar.gz");
         logger.println("{yellow+} bytes", db_tar_gz.size());
 
+        logger.print("Unzip database to");
+        auto db_tar = std::vector<uint8_t>(4 << 20); // result array in MBytes
+        z_stream zstream {};
+        zstream.next_in = db_tar_gz.data(); // input char array
+        zstream.avail_in = db_tar_gz.size(); // size of input
+        zstream.next_out = db_tar.data(); // output char array
+        zstream.avail_out = db_tar.capacity(); // size of output
+
+        // Add 32 to windowBits to enable zlib and gzip decoding with automatic
+        // header detection, or add 16 to decode only the gzip format.
+        // Magic number 32 = enable zlib and gzip decoding with automatic header detection
+        auto result = inflateInit2(&zstream, 32);
+        if (result != Z_OK)
+            throw std::runtime_error("ZLIB open error");
+
+        result = inflate(&zstream, Z_FINISH);
+        if (result != Z_STREAM_END)
+            throw std::runtime_error { "ZLIB " + std::string(zstream.msg) };
+        logger.println("{yellow+} bytes", zstream.total_out);
+
+        auto file_names = std::vector<std::string> {};
+        auto tar = mtar_t {};
+        auto tar_hdr = mtar_header_t {};
+        tar.stream = db_tar.data();
+        tar.seek = [](mtar_t*, unsigned) -> int { return MTAR_ESUCCESS; };
+        tar.close = [](mtar_t*) -> int { return MTAR_ESUCCESS; };
+        tar.read = [](mtar_t* tar, void* data, unsigned size) -> int {
+            std::memcpy(data, reinterpret_cast<uint8_t*>(tar->stream) + tar->pos, size);
+            return MTAR_ESUCCESS;
+        };
+        while ((mtar_read_header(&tar, &tar_hdr)) != MTAR_ENULLRECORD) {
+            file_names.emplace_back(tar_hdr.name);
+            mtar_next(&tar);
+        }
+
+        for (auto value = file_names.begin(); value < file_names.begin() + 4; value++)
+            logger.println("{}", *value);
+
+        // mtar_find(&tar, "test.txt", &h);
+        // p = calloc(1, h.size + 1);
+        // mtar_read_data(&tar, p, h.size);
+        // printf("%s", p);
+        // free(p);
+
         // find all not empty packages
         // for (auto const& pkg : ini.get_child(repo_name)) {
         //     auto const& pkg_name = pkg.first;
@@ -72,9 +118,6 @@ try {
         //     logger.println("{} -> {}", pkg_name, pkg_files);
         // }
     }
-
-    // https://github.com/nmoinvaz/minizip
-    // https://github.com/rxi/microtar
 
     // mingw-w64-x86_64-*-\\d
 
